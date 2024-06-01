7617a1d0f4c866bdbd88c086af755b72058ac5e2
Advik <advik.b@gmail.com>
2024-06-01 13:41:44 +0000

    m

--- api/index.py
+++ api/index.py
@@ -1,21 +1,17 @@
 from flask import Flask, request, Response
 import requests
 from flask_cors import CORS
-from diskcache import Cache
+# from diskcache import Cache
 from .config import target_url
 
 app = Flask(__name__)
 CORS(app)
 
-cache = Cache('webcache')
+# cache = Cache('webcache')
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>', methods=['GET', 'POST', 'PUT', 'DELETE'])
 def reverse_proxy(path):
-    if "asset" in path:
-        response = cache.get(path)
-        if response:
-            return response
     # Forward the request to the target URL
     url = target_url + '/' + path
     headers = {key: value for (key, value) in request.headers if key != 'Host'}
@@ -41,8 +37,6 @@ def reverse_proxy(path):
         resp.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE'
         resp.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
 
-    if "assets" in path:
-        cache.set(path, resp)
     return resp
 
 
